5d13855122fa744b6b4befe56ee11726d08f53e3
Daniel <danieldcc.colares@gmail.com>
2026-05-30 10:49:29 -0300

    chore: create projet component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,20 @@
+interface Props {
+  title: string;
+}
+
+const Component = ({ title }: Props) => {
+  return (
+    <>
+      <h2>Component</h2>
+    </>
+  );
+};
+
 export default function Home() {
-  return <h2>Pet shop</h2>;
+  return (
+    <div>
+      <h2>Pet shop</h2>
+      <Component title={1} />
+    </div>
+  );
 }
