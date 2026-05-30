6373635aab8eb2ff0934611fc4ce60289eb8c5be
Daniel <danieldcc.colares@gmail.com>
2026-05-30 11:08:13 -0300

    chore: removing unused component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,21 +1,7 @@
-interface Props {
-  title: string;
-  content: string;
-}
-
-const Component = ({ title, content }: Props) => {
-  return (
-    <>
-      <h2>Component</h2>
-    </>
-  );
-};
-
 export default function Home() {
   return (
     <div>
       <h2>Pet shop</h2>
-      <Component title={1} content={1} />
     </div>
   );
 }
